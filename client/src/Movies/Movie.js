80ac80dac8ca91809cdbd7c9eeb5c62c77a3910b
Jacqueline Omollo <jacquelineOmolloo@gmail.com>
2019-08-11 00:19:24 -0400

    MVP done

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,25 +1,24 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
-const Movie = (props) => {
-  const [movie, setMovie] = useState({});
- 
+const Movie = props => {
+  const [movie, setMovie] = useState(null);
+  const id = props.match.params.id;
   useEffect(() => {
-    const id = 1;
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
 
-       axios
-        .get(`http://localhost:5000/api/movies/${id}`)
-        .then(response => {
-          setMovie(response.data);
-        })
-        .catch(error => {
-          console.error(error);
-        });
+    axios
+      .get(`http://localhost:5000/api/movies/${id}`)
+      .then(response => {
+        setMovie(response.data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, [id]);
 
-  },[]);
-  
   // Uncomment this only when you have moved on to the stretch goals
   // const saveMovie = () => {
   //   const addToSavedList = props.addToSavedList;
@@ -35,6 +34,7 @@ const Movie = (props) => {
     <div className="save-wrapper">
       <div className="movie-card">
         <h2>{title}</h2>
+        <Link to="/movies/3">Next Movie</Link>
         <div className="movie-director">
           Director: <em>{director}</em>
         </div>
@@ -52,6 +52,6 @@ const Movie = (props) => {
       <div className="save-button">Save</div>
     </div>
   );
-}
+};
 
 export default Movie;
